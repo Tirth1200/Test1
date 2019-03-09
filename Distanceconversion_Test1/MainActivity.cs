0db92ec572dccc54774948c3c6eb1b3fbbc99368
visualstudio <visualstudio@123-PC41>
2019-03-09 16:19:40 -0500

    FInal Commit

--- Distanceconversion_Test1/MainActivity.cs
+++ Distanceconversion_Test1/MainActivity.cs
@@ -12,18 +12,58 @@ namespace Distanceconversion_Test1
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        EditText etOutput, etInput;
+        RadioButton rdbKM, rdbMK, rdbCI, rdbIC;
+        Button btnConvert;
+        double result;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+            etOutput = (EditText)FindViewById(Resource.Id.etout);
+            etInput = (EditText)FindViewById(Resource.Id.etin);
+            rdbMK = (RadioButton)FindViewById(Resource.Id.rdMK);
+            rdbKM = (RadioButton)FindViewById(Resource.Id.rdKM);
+            rdbCI = (RadioButton)FindViewById(Resource.Id.rdCI);
+            rdbIC = (RadioButton)FindViewById(Resource.Id.rdIC);
+            btnConvert = (Button)FindViewById(Resource.Id.btConvert);
+            btnConvert.Click += delegate
+            {
+                if (etInput.Text != "")
+                {
+                    double input = double.Parse(etInput.Text);
+              
+                    if (rdbKM.Checked)
+                    {
+                        result = input * 0.621371;
+                    }
+                    else if (rdbMK.Checked)
+                    {
+                        result = input * 1.60934;
+                    }
+                    else if (rdbCI.Checked)
+                    {
+                        result = input * 0.393701;
+                    }
+                    else if (rdbIC.Checked)
+                    {
+                        result = input * 2.54;
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Please select the type of conversion", ToastLength.Long).Show();
+                    }
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+                    etOutput.Text = result.ToString();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Please enter an Input", ToastLength.Long).Show();
+                }
+                
+            };
         }
+            
 
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
